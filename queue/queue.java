ff61abb3e64359fe2458ee70c7892936557dd36f
Selden Lin <selden.lin@gmail.com>
2019-10-04 00:01:04 -0400

    Started implementation of queue with array

---
+++ queue/queue.java
@@ -0,0 +1,13 @@
+public class Queue<T> {
+  private T queue[];
+  private int front = 0;
+  public Queue(int size) {
+    this.queue = new T[size];
+  }
+  public Queue() {
+    this.queue = new T[50];
+  }
+  public dequeue() {
+    if(this.front == 0) {
+  }
+}
